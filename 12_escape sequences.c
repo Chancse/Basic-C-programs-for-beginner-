db31e8432884ef6ef9496259118c283836bb9bed
Chancse <61348662+Chancse@users.noreply.github.com>
2020-08-13 17:49:41 +0530

    Add files via upload

---
+++ 12_escape sequences.c
@@ -0,0 +1,11 @@
+#include<stdio.h> //without using the following method we can't print them usually
+int main()
+{
+    printf("To print \\n");//to print \n we need to give \\n
+    printf("\n");// \n is used to print new line
+    printf("To print backslash \\ ");//to print \ we need to give double backslash
+    printf("\n");
+    printf("To print single r double quotes\n\'\'  \"\"");//to print single or double quotes we need to give backslash before them
+    printf("\n");
+    return 0;
+}
